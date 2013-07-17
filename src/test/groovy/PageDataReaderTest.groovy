816b159f779732775c692101bdda4111da3d506c
urasa <ursk4git@live.jp>
2013-07-18 01:43:10 +0900

    一時ファイル作成処理をまとめた

--- src/test/groovy/PageDataReaderTest.groovy
+++ src/test/groovy/PageDataReaderTest.groovy
@@ -8,36 +8,34 @@ class PageDataReaderTest {
     @Rule
     public TemporaryFolder folder = new TemporaryFolder()
 
-    @Test
-    void ファイルからプロパティprop1とprop2及びクロージャclosを読み取るreaderをつくれる() {
+    File createTempConfigFile() {
         File configFile = folder.newFile('simpleReader.groovy')
         configFile.withPrintWriter { writer ->
             writer.println "properties = ['prop1', 'prop2']"
             writer.println "clos = { -> 'clos from configFile'}"
         }
+        configFile
+    }
+    @Test
+    void ファイルからプロパティprop1とprop2及びクロージャclosを読み取るreaderをつくれる() {
+        File configFile = createTempConfigFile()
         PageDataReader sut = PageDataReader.createReader(configFile)
         assertThat(sut.propertiesToRead, is(['prop1','prop2']))
         assertThat(sut.getPageStringClos.call(), is('clos from configFile'))
     }
     @Test
     void 設定ファイルからpropertiesを読み取れる() {
-        File configFile = folder.newFile('simpleReader.groovy')
-        configFile.withPrintWriter { writer ->
-            writer.println "properties = ['prop1', 'prop2']"
-        }
+        File configFile = createTempConfigFile()
         def readerConfig = new ConfigSlurper().parse(configFile.toURL())
         def actual = PageDataReader.readPropertyList readerConfig
         assertThat actual, is(['prop1','prop2'])
     }
     @Test
     void 設定ファイルからclosを読み取れる() {
-        File configFile = folder.newFile('simpleReader.groovy')
-        configFile.withPrintWriter { writer ->
-            writer.println "clos = { -> 'this is a test'}"
-        }
+        File configFile = createTempConfigFile()
         def readerConfig = new ConfigSlurper().parse(configFile.toURL())
         def actual = PageDataReader.readGetPageStringClos readerConfig
-        assertThat actual.call(), is('this is a test')
+        assertThat actual.call(), is('clos from configFile')
     }
     @Test
     void resourceにおいた設定ファイルから読み取れる() {
