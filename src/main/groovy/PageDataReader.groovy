60f3e184de155b4151b3014287eeffe5330e8214
urasa <ursk4git@live.jp>
2013-07-18 00:57:08 +0900

    PageDataのさくせい

---
+++ src/main/groovy/PageDataReader.groovy
@@ -0,0 +1,39 @@
+import groovy.transform.Immutable
+@Immutable
+class PageDataReader {
+    List propertiesToRead
+    Closure getPageStringClos
+
+    /**
+    * scriptからreaderが読むべきプロパティのリストと
+    * 読んだプロパティからページのテキストを生成するクロージャを取得し
+    * それに基づいてファイルからPageDataを生成するPageDataReaderを作成する
+    */
+    static PageDataReader createReader(File readerSetupScript) {
+        if (readerSetupScript == null) {
+            throw new IllegalArgumentException('argument is null')
+        }
+        def readerConfig = new ConfigSlurper().parse(readerSetupScript.toURL())
+        List properties = readPropertyList readerConfig
+        Closure clos = readGetPageStringClos readerConfig
+        new PageDataReader(propertiesToRead:properties, getPageStringClos:clos)
+        //new PageDataReader(['prop1','prop2'], {})
+    }
+    static List readPropertyList(ConfigObject config) {
+        config.properties
+    }
+    static Closure readGetPageStringClos(ConfigObject config) {
+        config.clos
+    }
+    /**
+    * ページ作成に必要なプロパティをファイルから読み込み
+    * PageDataを作成する
+    */
+    PageData readPropertiesAndCreatePageData(File src) {
+        // propertiesToReadに指定されたpropertyを
+        // srcから読み込んでpropertiesに格納する
+        Map properties
+        new PageData(properties, getPageStringClos)
+    }
+}
+
---
+++ src/test/groovy/PageDataReaderTest.groovy
@@ -0,0 +1,44 @@
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+class PageDataReaderTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder()
+
+    @Test
+    void ファイルからプロパティprop1とprop2及びクロージャclosを読み取るreaderをつくれる() {
+        File configFile = folder.newFile('simpleReader.groovy')
+        configFile.withPrintWriter { writer ->
+            writer.println "properties = ['prop1', 'prop2']"
+            writer.println "clos = { -> 'clos from configFile'}"
+        }
+        PageDataReader sut = PageDataReader.createReader(configFile)
+        
+        assertThat(sut.propertiesToRead, is(['prop1','prop2']))
+        assertThat(sut.getPageStringClos.call(), is('clos from configFile'))
+    }
+    @Test
+    void 設定ファイルからpropertiesを読み取れる() {
+        File configFile = folder.newFile('simpleReader.groovy')
+        configFile.withPrintWriter { writer ->
+            writer.println "properties = ['prop1', 'prop2']"
+        }
+        def readerConfig = new ConfigSlurper().parse(configFile.toURL())
+        def actual = PageDataReader.readPropertyList readerConfig
+        assertThat actual, is(['prop1','prop2'])
+    }
+    @Test
+    void 設定ファイルからclosを読み取れる() {
+        File configFile = folder.newFile('simpleReader.groovy')
+        configFile.withPrintWriter { writer ->
+            writer.println "clos = { -> 'this is a test'}"
+        }
+        def readerConfig = new ConfigSlurper().parse(configFile.toURL())
+        def actual = PageDataReader.readGetPageStringClos readerConfig
+        assertThat actual.call(), is('this is a test')
+    }
+}
+
